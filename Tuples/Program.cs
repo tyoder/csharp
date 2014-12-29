2e43977d0a6daeb28d76528993eb35a2478acfbb
tyoder <tkyoder93@gmail.com>
2014-12-29 12:40:38 -0500

    Creating Tuples and Sorting lists of Tuples

--- Tuples/Program.cs
+++ Tuples/Program.cs
@@ -1,7 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Tuples
 {
+    /// <summary>
+    /// A Tuple is a class, not a struct
+    /// It is excellent as a short-term container.
+    /// The properties of a Tuple (Item1, Item2, etc) do not have setters.
+    /// Values of properties can only be set upon construction - they are immutable
+    /// Reference:  http://www.dotnetperls.com/tuple
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
@@ -14,8 +23,25 @@ namespace Tuples
 
             // pass tuple as an argument
             WriteTuple(tuple);
+
+            // Tuple.Create
+            var createdTuple = Tuple.Create("cat", 2, true);
+
+            WriteCreatedTuple(createdTuple);
+
+            //SortTupleList();
+
+            SortTupleWithLinq();
+
             Console.Read();
-    }
+        }
+
+        static void WriteCreatedTuple(Tuple<string, int, bool> myCreatedTuple)
+        {
+            Console.WriteLine(myCreatedTuple.Item1);
+            Console.WriteLine(myCreatedTuple.Item2);
+            Console.WriteLine(myCreatedTuple.Item3);
+        }
 
         static void WriteTuple(Tuple<string, string[], int, int[]> myTuple)
         {
@@ -29,6 +55,42 @@ namespace Tuples
             {
                 Console.WriteLine(value);
             }
+        }
+
+        static void SortTupleList()
+        {
+            var tupleList = new List<Tuple<int, string>>();
+            tupleList.Add(new Tuple<int, string>(1, "cat"));
+            tupleList.Add(new Tuple<int, string>(100, "apple"));
+            tupleList.Add(new Tuple<int, string>(2, "zebra"));
+
+            // Use Sort method with Comparison delegate.
+            tupleList.Sort((a,b) => a.Item2.CompareTo(b.Item2));
+
+            foreach (var element in tupleList)
+            {
+                Console.WriteLine(element);
+            }
+            
+        }
+
+        static void SortTupleWithLinq()
+        {
+            var tupleList = new List<Tuple<int, string>>();
+            tupleList.Add(new Tuple<int, string>(1, "cat"));
+            tupleList.Add(new Tuple<int, string>(100, "apple"));
+            tupleList.Add(new Tuple<int, string>(2, "zebra"));
+
+            var tupleListSorted = from thing in tupleList
+                orderby thing.Item1
+                select thing;
+
+            foreach (var tupleThing in tupleListSorted)
+            {
+                Console.WriteLine(tupleThing);
+            }
+
+
 
         }
     }
