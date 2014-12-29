91ac4255072581f90030bdd21f4c06ac7a5e7d10
tyoder <tkyoder93@gmail.com>
2014-12-29 11:38:52 -0500

    Added Tuples console

---
+++ Tuples/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tuple = new Tuple<string, string[], int, int[]>(
+                "perl",
+                new string[] {"java", "c#"},
+                1,
+                new int[] {2,3});
+
+            // pass tuple as an argument
+            WriteTuple(tuple);
+            Console.Read();
+    }
+
+        static void WriteTuple(Tuple<string, string[], int, int[]> myTuple)
+        {
+            Console.WriteLine(myTuple.Item1);
+            foreach(string value in myTuple.Item2)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine(myTuple.Item3);
+            foreach(int value in myTuple.Item4)
+            {
+                Console.WriteLine(value);
+            }
+
+        }
+    }
+}
